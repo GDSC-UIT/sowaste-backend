9928afaeeacd59880b5db777796623f3cdaac9a3
LamCam <lamcam813@gmail.com>
2023-01-13 00:36:21 +0700

    database

--- go/cmd/app/main.go
+++ go/cmd/app/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
+
 	fmt.Println("Hello world")
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Some error occured. Err: %s", err)
+	}
+
+	// client.Db.ConnectDb()
 }
