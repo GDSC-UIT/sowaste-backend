5c49b294680a9f72368d21a1cc772a2d6e0bd858
Tuan Kiet Tran <charlieismylife2003@gmail.com>
2023-01-16 12:29:26 +0700

    bottle shop struct edit

--- go/internal/model/bottle-shop.model.go
+++ go/internal/model/bottle-shop.model.go
@@ -3,14 +3,14 @@ package model
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Location struct {
-	X              int32  `bson:"x" json:"x"`
-	Y              int32  `bson:"y" json:"y"`
-	LocationString string `bson:"location_string" json:"location_string"`
+	X              float32 `bson:"x" json:"x"`
+	Y              float32 `bson:"y" json:"y"`
+	LocationString string  `bson:"location_string" json:"location_string"`
 }
 
 type BottleShop struct {
 	ID       primitive.ObjectID `bson:"_id" json:"_id"`
 	Location Location           `bson:"location" json:"location"`
 	Status   string             `bson:"status" json:"status"`
-	Point    int64              `bson:"point" json:"point"`
+	Rate     uint8              `bson:"rate" json:"rate"`
 }
