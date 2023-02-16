342632b678a3447b3f9e0cfddfecc83925d6a916
Tuan Kiet Tran <charlieismylife2003@gmail.com>
2023-02-16 13:47:03 +0700

    Edit db to serve in local user

--- go/internal/model/bottle-shop.model.go
+++ go/internal/model/bottle-shop.model.go
@@ -3,10 +3,11 @@ package model
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Location struct {
-	X              float32 `bson:"x" json:"x"`
-	Y              float32 `bson:"y" json:"y"`
+	Latitude       float32 `bson:"latitude" json:"latitude"`
+	Longtitude     float32 `bson:"longtitude" json:"longtitude"`
 	LocationString string  `bson:"location_string" json:"location_string"`
 	LocationMap    string  `bson:"location_map" json:"location_map"`
+	Locale         string  `bson:"locale" json:"locale"`
 }
 
 type BottleShop struct {
@@ -14,4 +15,6 @@ type BottleShop struct {
 	Location Location           `bson:"location" json:"location"`
 	Status   string             `bson:"status" json:"status"`
 	Rate     uint8              `bson:"rate" json:"rate"`
+	Contact  string             `bson:"contact" json:"contact"`
+	Images   []string           `bson:"images" json:"images"`
 }
--- go/internal/model/dictionary.model.go
+++ go/internal/model/dictionary.model.go
@@ -5,7 +5,8 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 type Dictionary struct {
 	ID               primitive.ObjectID `bson:"_id" json:"_id"`
 	Name             string             `bson:"name" json:"name"`
-	Type             string             `bson:"type" json:"type"`
+	IsOrganic        bool               `bson:"is_organic" json:"is_organic"`
+	Recyable         bool               `bson:"recyable" json:"recyable"`
 	ShortDescription string             `bson:"short_description" json:"short_description"`
 	Description      string             `bson:"description" json:"description"`
 	Uri              string             `bson:"uri" json:"uri"`
